eb240dc52a94e3fa0f999e2a5aba839809053f71
knowledge <githubtech13@gmail.com>
2019-06-30 12:33:20 +0530

    demo project os committed

---
+++ Demo/src/com/demo/src/Gfg.java
@@ -0,0 +1,21 @@
+package com.demo.src;
+
+public class Gfg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				int a = 2; 
+				for (;;) { 
+
+					if (a == 2) { 
+
+						break; 
+					} 
+				} 
+			} 
+		} 
+
+	
+
+
